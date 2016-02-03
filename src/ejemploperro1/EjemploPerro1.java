f5de3f0371e95ef47c2f3d309fc3a3b988d921bd
carlo <carlo@10.20.25.67>
2016-02-03 08:02:50 -0500

    InicialGit

---
+++ src/ejemploperro1/EjemploPerro1.java
@@ -0,0 +1,15 @@
+package ejemploperro1;
+
+/**
+ *
+ * @author carlo
+ */
+public class EjemploPerro1 {
+ private int edad;
+ private String nombre;
+ private String raza;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
